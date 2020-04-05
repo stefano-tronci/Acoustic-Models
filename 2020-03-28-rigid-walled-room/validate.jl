988e931334e0a8dd41a9640de01ab259d89879f7
CrocoDuck <CrocoDuckoDucks@users.noreply.github.com>
2020-04-05 11:41:19 +0100

    Added model in corner, better error analysis

--- 2020-03-28-rigid-walled-room/validate.jl
+++ 2020-03-28-rigid-walled-room/validate.jl
@@ -1,4 +1,5 @@
 using DelimitedFiles
+using Statistics
 using Plots
 
 include("exact.jl")
@@ -11,11 +12,17 @@ Lz = 3
 
 N = 10
 
+t = 0
+
 F = modeFrequency.(A, B, C, Lx, Ly, Lz)
 
 idx = sortperm(F[:])
 
 f = F[idx[2:(N + 1)]]
+
+for n in 1:length(f)
+    writedlm("frequencies/f_$n.csv", f[n])
+end
 
 a = A[idx[2:(N + 1)]]
 b = B[idx[2:(N + 1)]]
@@ -27,7 +34,10 @@ for d in 0:(N - 1)
 
     linedata = readdlm("export/data_$d.csv", ',')
 
-    femSolution = Float64.(linedata[2:end, 2])
+    femSolution = real.(
+        (Float64.(linedata[2:end, 1]) .+ im * Float64.(linedata[2:end, 2])) *
+        exp(im * 2π * f[d + 1] * t)
+    )
 
     x = Float64.(linedata[2:end, 3])
     y = Float64.(linedata[2:end, 4])
@@ -37,7 +47,7 @@ for d in 0:(N - 1)
         x .+ 0.5 * Lx, y .+ 0.5 * Ly, z .+ 0.5 * Lz,
         a[d + 1], b[d + 1], c[d + 1],
         Lx, Ly, Lz
-    )
+    ) * cospi(2 * f[d + 1] * t)
 
     me, ie = findmax(exactSolution)
 
@@ -62,7 +72,7 @@ bar(
     norms,
     legend=false,
     framestyle=:box,
-    xlabel="Mode frequency[Hz]",
+    xlabel="Mode frequency [Hz]",
     ylabel="Norm of the Error Field [Pa]",
     yscale=:log10,
     xticks=(1:N, round.(f, digits=3))
